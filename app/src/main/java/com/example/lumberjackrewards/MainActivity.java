c236b775ad4f221f09015907943be74911b0a9ee
ReggieMlay <reggiereal7@gmail.com>
2023-02-12 15:34:19 -0600

    created the design of the settings and what it could look like. we could add more items to it and/or remove some things, good to brainstorm about this

--- app/src/main/java/com/example/lumberjackrewards/MainActivity.java
+++ app/src/main/java/com/example/lumberjackrewards/MainActivity.java
@@ -3,12 +3,46 @@ package com.example.lumberjackrewards;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+
+import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    /*@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = (Button)findViewById(R.id.settings);
+
+        btn.setOnClickListener(new View.OnClickListener())
+        {
+            @Override
+                    public void onClick(View view){
+                Intent intent = new Intent(this, Settings.class)
+        }
+
+
+        }
+    }*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = (Button) findViewById(R.id.settings);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNewActivity();
+            }
+        });
+    }
+    public void openNewActivity(){
+        Intent intent = new Intent(this, Settings.class);
+        startActivity(intent);
     }
 }
