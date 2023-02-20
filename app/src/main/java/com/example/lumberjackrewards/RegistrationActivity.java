5a4e48441b36c4e743db24360168d96abd41e756
johnSoloQ <97631254+johnSoloQ@users.noreply.github.com>
2023-02-19 22:24:24 -0600

    Added User add and delete back and partial get with adaptations to RegistrationActivity.java to get it from txt box to database.

--- app/src/main/java/com/example/lumberjackrewards/RegistrationActivity.java
+++ app/src/main/java/com/example/lumberjackrewards/RegistrationActivity.java
@@ -1,4 +1,73 @@
 package com.example.lumberjackrewards;
 
-public class RegistrationActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    private EditText txtEmail;
+
+    private EditText txtPassword;
+
+    private Button btnRegister;
+
+    private TextView tvwLogin;
+
+    private FirebaseFirestore database;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_registration);
+
+        database = FirebaseFirestore.getInstance();
+
+        txtEmail = findViewById(R.id.RegistrationEmail);
+        txtPassword = findViewById(R.id.RegistrationPassword);
+        btnRegister = findViewById(R.id.RegistrationButton);
+        tvwLogin = findViewById(R.id.RegistrationPageQuestion);
+
+        tvwLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegistrationActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = txtEmail.getText().toString().trim();
+                String password = txtPassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)){
+                    txtEmail.setError("A valid email is required");
+                    return;
+                }
+                if (TextUtils.isEmpty(password)){
+                    txtPassword.setError("A valid password is required");
+                    return;
+                } else{
+                    UserModel user = new UserModel(null, null, email, "user",password);
+                    user.addNewUser(database);
+                }
+            }
+        });
+    }
 }
+
--- app/src/main/java/com/example/lumberjackrewards/UserModel.java
+++ app/src/main/java/com/example/lumberjackrewards/UserModel.java
@@ -1,11 +1,30 @@
 package com.example.lumberjackrewards;
 
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+
 public class UserModel {
     private String fName;
     private String lName;
     private String eMail;
     private String role;
+    private String password;
 
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
     public String getfName() {
         return fName;
     }
@@ -38,14 +57,60 @@ public class UserModel {
         this.role = role;
     }
 
-    public UserModel(String fName, String lName, String eMail, String role) {
+    public UserModel(String fName, String lName, String eMail, String role, String password) {
         this.fName = fName;
         this.lName = lName;
         this.eMail = eMail;
         this.role = role;
+        this.password = password;
     }
 
+    public void addNewUser(FirebaseFirestore db){
+        String documentID = this.geteMail().replaceAll("\\s", "");
 
+        // on below line are are adding a new user to the db
+        db.collection("users").document(documentID).set(this)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Log.d("NEW_USER", "Successfully added new user to db: " + this.toString());
+                    }
+                });
+    }
 
+    public void deleteUser(FirebaseFirestore db){
+        String documentID = this.geteMail().replaceAll("\\s", "");
 
+        // on below line are deleting a user to the db
+        db.collection("users").document(documentID).delete()
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Log.d("DELETE_USER", "Successfully deleted user from db: " + this.toString());
+                    }
+                });
+    }
+
+    public void getPassword(FirebaseFirestore db){
+        String documentID = this.geteMail().replaceAll("\\s", "");
+        CollectionReference usersRef = db.collection("users");
+        Query query = usersRef.whereEqualTo("email",this.eMail);
+
+        db.collection("users")
+                .whereEqualTo("email",this.eMail)
+                .get()
+                .addOnCompleteListener(new onCompleteListener<QuerySnapshot>()) {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                }
+        }
+    }
 }
