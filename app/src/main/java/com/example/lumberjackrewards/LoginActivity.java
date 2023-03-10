a2c8995d2b6a7dcbf06f6eafe02625e562096f16
jfglenn <jessieglenn2002@gmail.com>
2023-03-09 20:59:27 -0600

    added functionality to login and registration page. Not yet tested, but implemented

--- app/src/main/java/com/example/lumberjackrewards/LoginActivity.java
+++ app/src/main/java/com/example/lumberjackrewards/LoginActivity.java
@@ -1,4 +1,108 @@
 package com.example.lumberjackrewards;
 
-public class LoginActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+    private EditText loginEmail, loginPwd;
+    private Button loginBtn;
+    private TextView loginQn;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
+
+
+        // If user is already logged in already go to homepage
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = mAuth.getCurrentUser();
+                if (user != null){
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+
+        loginEmail = findViewById(R.id.loginEmail);
+        loginPwd = findViewById(R.id.loginPassword);
+        loginBtn = findViewById(R.id.loginButton);
+        loginQn = findViewById(R.id.loginPageQuestion);
+
+        // Redirect user to registration page if
+        // account has not already been created
+        loginQn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, RegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Authenticate user and login to home page
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = loginEmail.getText().toString().trim();
+                String password = loginPwd.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    loginEmail.setError("A valid email is required");
+                    return;
+                }
+                if (TextUtils.isEmpty(password)){
+                    loginPwd.setError("A valid password is required");
+                    return;
+                } else{
+
+                    mAuth.signInWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()){
+                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                startActivity(intent);
+                                finish();
+                            } else{
+                                String error = task.getException().toString();
+                                Toast.makeText(LoginActivity.this, "Login failed: " + error, Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mAuth.removeAuthStateListener(authStateListener);
+    }
 }
--- app/src/main/java/com/example/lumberjackrewards/RegistrationActivity.java
+++ app/src/main/java/com/example/lumberjackrewards/RegistrationActivity.java
@@ -1,4 +1,88 @@
 package com.example.lumberjackrewards;
 
-public class RegistrationActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegistrationActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+
+    private EditText RegEmail, RegPwd;
+    private Button RegBtn;
+    private TextView RegQn;
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+        RegEmail = findViewById(R.id.RegistrationEmail);
+        RegPwd = findViewById(R.id.RegistrationPassword);
+        RegBtn = findViewById(R.id.RegistrationButton);
+        RegQn = findViewById(R.id.RegistrationPageQuestion);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        // Redirect user to login page if
+        // account has already been created
+        RegQn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegistrationActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Register user in the system
+        RegBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // removes whitespace from both ends of the strings
+                String email = RegEmail.getText().toString().trim();
+                String password = RegPwd.getText().toString().trim();
+
+                // ensure fields are filled out
+                if (TextUtils.isEmpty(email)){
+                    RegEmail.setError("A valid email is required");
+                    return;
+                }
+                if (TextUtils.isEmpty(password)){
+                    RegPwd.setError("A valid password is required");
+                    return;
+                } else{
+                    /*loader.setMessage("Registration in progress");
+                    loader.setCanceledOnTouchOutside(false);
+                    loader.show();*/
+                    mAuth.createUserWithEmailAndPassword(email,password)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            if(task.isSuccessful()){
+                                Intent intent = new Intent(RegistrationActivity.this, MainActivity.class);
+                                startActivity(intent);
+                                finish();
+                            } else{
+                                String error = task.getException().toString();
+                                Toast.makeText(RegistrationActivity.this, "Registration failed: " + error, Toast.LENGTH_SHORT).show();
+                            }
+                            //loader.dismiss();
+                        }
+                    });
+                }
+            }
+        });
+    }
 }
